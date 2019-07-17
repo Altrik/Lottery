4b84e62eb845bfc43b8d7a9b5bc1a9a118b34ef3
Altrik <tiger_two@wp.pl>
2019-07-17 18:59:38 +0200

    Set up project on github

---
+++ src/main/java/pl/mysite/app/AppConfig.java
@@ -0,0 +1,72 @@
+package pl.mysite.app;
+
+import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.mysite")
+//@EnableJpaRepositories(basePackages = "pl.mysite.repository")
+@EnableTransactionManagement
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+//	@Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//          .addResourceHandler("/resources/**")
+//          .addResourceLocations("/resources/")
+//          .setCachePeriod(3600)
+//          .resourceChain(true)
+//          .addResolver(new PathResourceResolver());
+//    }
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public LocalEntityManagerFactoryBean entityManagerFactory() {
+		LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+		emfb.setPersistenceUnitName("lotteryPersistenceUnit"); 
+		return emfb;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager tm = new JpaTransactionManager(emf);
+		return tm;
+	}
+
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+
+}
+
